7f75befc51f7d7391df8ce6e9defc387af55db3c
unknown <teddysmithdev@gmail.com>
2020-03-04 11:38:15 -0500

    please god deploy

--- home/wsgi/prod.py
+++ home/wsgi/prod.py
@@ -1,9 +1,11 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
+
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
+
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
