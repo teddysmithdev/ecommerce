fc901e581ef9f236ca93e416f70d3fe52f9eb980
unknown <teddysmithdev@gmail.com>
2020-03-04 10:34:19 -0500

    make it better3

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
@@ -10,7 +10,6 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls'))
-    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
