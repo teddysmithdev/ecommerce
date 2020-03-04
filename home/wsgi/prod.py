67bc61211591df68a15cf5ecd64ba026e18ab953
unknown <teddysmithdev@gmail.com>
2020-03-04 14:25:57 -0500

    asbbf

--- home/wsgi/prod.py
+++ home/wsgi/prod.py
@@ -1,11 +1,12 @@
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
 
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.prod")
 
 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
